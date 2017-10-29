d3136c9cc0a2c10f685fe930b8a1f3bfb7c58d83
Brent Rojas <brentrojas@users.noreply.github.com>
2017-10-28 18:42:04 -0700

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'Django-Server'
+maintainer 'DynaCode'
+maintainer_email ''
+license 'apache'
+description 'Installs/Configures a Django-based server'
+long_description 'Installs/Configures a Django-based server'
+version '0.1'
+
+#depends 'apache2', '~> 3.2.2'
