da99468602c40e3e5db980161934f81df69623f5
Brent Rojas <brentrojas@users.noreply.github.com>
2017-10-29 12:34:51 -0700

    Create metadata.rb

---
+++ opsworks_demo/metadata.rb
@@ -0,0 +1,7 @@
+name 'OpsWorks Demo'
+maintainer 'DynaCode'
+maintainer_email ''
+license 'apache'
+description 'Installs/Configures a Django-based server'
+long_description 'Installs/Configures a Django-based server'
+version '0.1'
