443ddb037176caf8eb1ffaa2af9d3b4353843f00
Brent Rojas <brentrojas@users.noreply.github.com>
2017-10-29 15:20:49 -0700

    Update metadata.rb

--- opsworks_demo/metadata.rb
+++ opsworks_demo/metadata.rb
@@ -1,7 +1,14 @@
-name 'OpsWorks Demo'
+name 'opsworks_demo'
 maintainer 'DynaCode'
 maintainer_email ''
-license 'apache'
+license 'Apache 2.0'
 description 'Installs/Configures a Django-based server'
-long_description 'Installs/Configures a Django-based server'
 version '0.1'
+
+recipe "opsworks_demo::create_directory", "Create the /opt/django direcotry"
+
+#name        "deploy"
+#description "Deploy applications"
+#maintainer  "AWS OpsWorks"
+#license     "Apache 2.0"
+#version     "1.0.0"
